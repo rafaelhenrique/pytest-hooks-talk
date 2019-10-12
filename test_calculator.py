482e6e0be6ba417a17e511ab241b1c7156f17d62
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2019-10-12 18:49:30 -0300

    Fix tests

--- test_calculator.py
+++ test_calculator.py
@@ -2,12 +2,12 @@ import calculator
 
 
 def test_sum():
-    return calculator.sum(1, 1) == 2
+    assert calculator.sum(1, 1) == 2
 
 
 def testa_mul():
-    return calculator.mul(1, 1) == 1
+    assert calculator.mul(1, 1) == 1
 
 
 def testei_div():
-    return calculator.div(1, 1) == 1
+    assert calculator.div(1, 1) == 1
